201561e2d2d5188309c7e203fb01ca25e1ce2fd8
Alex Ding <dingjiev@hotmail.com>
2015-09-27 13:39:01 -0400

    fill in the function body

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions compute and cache the inverse of a matrix under
+## two scenarios. 1) If the inverse of a matrix has not been computed, the
+## inverse will be computed and cached. 2) If the inverse of a matrix 
+## has been computed, the inverse can be looked up in the cache and returned 
+## rather than being recomputed.
 
-## Write a short comment describing this function
+## The fucntion makeCacheMatrix takes an invertible numeric matrix as input and 
+## returns a sepecial "matrix", which is really a list containing a function to: 
+## 1) set the value of the matrix 
+## 2) get the value of the matrix
+## 3) set the inverse of the matrix
+## 4) get the inverse of the matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+            x <<- y
+            inv <<- NULL 
+        }
+        get <- function() x
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve computes the inverse of the special "matrix" object
+## returned by makeCacheMatrix above. If the inverse has been computed, it 
+## retrieves the inverse from the cache and skips the computation. Otherwise, 
+## it computes the inverse of the matrix and sets the inverse in the caches.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
